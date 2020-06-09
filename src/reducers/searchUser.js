53b4cbc433ce6fadb1d7ed197417a799f0aabc25
Joeyhiew <44110275+Joeyhiew@users.noreply.github.com>
2020-06-09 17:23:34 +0800

    Addressed Edge compatibility issues

--- src/reducers/searchUser.js
+++ src/reducers/searchUser.js
@@ -3,81 +3,115 @@ import initialState from './initialState';
 
 const searchUser = (state = initialState, action) => {
     switch (action.type) {
-        case Types.TO_SEARCH :
-            return {
-                ...state,
-                text: action.payload
-            };
+        case Types.TO_SEARCH:
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    text: action.payload
+                }
+            )
+
         case Types.FETCH_REPO_BEGIN:
-            return {
-                ...state,
-                loading: true,
-                repoError: null
-            };
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: true, 
+                    repoError: null
+                }
+            )
 
         case Types.FETCH_REPO_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                repo: action.payload.products
-            };
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: false, 
+                    repo: action.payload.products
+                }
+            )
 
         case Types.FETCH_REPO_FAILURE:
-            return {
-                ...state,
-                loading: false,
-                repoError: action.payload.error,
-                repo: []
-            };
-            case Types.FETCH_ORG_BEGIN:
-                return {
-                    ...state,
-                    loading: true,
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: false, 
+                    repo: [], 
+                    repoError: action.payload.error
+                }
+            )
+
+        case Types.FETCH_ORG_BEGIN:
+            return  Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: true, 
                     orgError: null
-                };
-    
-            case Types.FETCH_ORG_SUCCESS:
-                return {
-                    ...state,
-                    loading: false,
+                }
+            )
+
+        case Types.FETCH_ORG_SUCCESS:
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: false, 
                     org: action.payload.products
-                };
-    
-            case Types.FETCH_ORG_FAILURE:
-                return {
-                    ...state,
-                    loading: false,
-                    orgError: action.payload.error,
-                    org: []
-                };
-            case Types.CHECK_USER_BEGIN:
-                return {
-                    ...state,
-                    loading: true,
-                    error: null,
-                    userFound: null,
-                    profile: [],
-                };
-    
-            case Types.CHECK_USER_SUCCESS:
-                return {
-                    ...state,
-                    loading: false,
-                    profile: action.payload.products,
+                }
+            )
+
+        case Types.FETCH_ORG_FAILURE:
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: false, 
+                    org: [], 
+                    orgError: action.payload.error
+                }
+            )
+
+        case Types.CHECK_USER_BEGIN:
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: true, 
+                    error: null, 
+                    userFound: null, 
+                    profile: []
+                }
+            )
+
+
+        case Types.CHECK_USER_SUCCESS:
+            return Object.assign(
+                {}, 
+                state, 
+                {
+                    loading: false, 
+                    profile: action.payload.products, 
                     userFound: true
-                };
-    
-            case Types.CHECK_USER_FAILURE:
-                return {
-                    ...state,
-                    loading: false,
-                    error: action.payload.error,
-                    profile: [],
+                }
+            )
+
+        case Types.CHECK_USER_FAILURE:
+            return Object.assign(
+                {},
+                state,
+                {
+                    loading: false, 
+                    error: action.payload.error, 
+                    profile: [], 
                     userFound: false
-                };
+                }
+            )
             
         default:
             return state;
-    }
-}
+    };
+};
 export default searchUser;
--- src/store.js
+++ src/store.js
@@ -5,7 +5,6 @@ import thunk from "redux-thunk";
 const store = createStore(
     allReducer,
     compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)
 );
 export default store;
